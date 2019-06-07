8f8d1eea050eed8c9b8311f4d6f4725b3977d92b
Devyanshi Tiwari <devyanshitiwari@Devyanshis-MacBook-Pro.local>
2019-06-07 14:56:51 +0530

    2nd commit

--- Prog1.java
+++ Prog1.java
@@ -9,7 +9,7 @@ public class Prog1 {
 			int count=0;
 			Scanner sc=new Scanner(System.in);
 			System.out.println("Input 5 strings");
-			for(int i=0;i<5;i++)//input the values from user
+			for(int i=0;i<5;i++)
 			{
 				
 				arr[i]=sc.next();
@@ -19,19 +19,17 @@ public class Prog1 {
 			    temp=arr[i];
 			    StringBuffer sb=new StringBuffer(temp);
 			    temp2=new String(sb.reverse());
-			    if(temp2.contentEquals(temp))
+			    if(temp2.equalsIgnoreCase(temp))
 			    {	count++;
 			    System.out.println(" Found a palindrome "+temp);
-			    }
-			    
-			   
+			    }			      
 			    
 			}
 			System.out.println("Count of palindrome strings = " +count);
 			
-		}
+		   }
 			
 			
 			
 		
-}
+        }
